01c064cc36f0b792dd1b295010e5cb20b8cbb0b8
sivm99 <90413888+sivm99@user.noreply.github.com>
2024-08-07 13:25:36 +0530

    hard coded props support

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import './App.css'
-import Header from './components/Header'
-import Section from './components/Section'
+import "./App.css";
+import Header from "./components/Header";
+import Section from "./components/Section";
 
-
-function App(props){
+function App() {
   return (
     <div>
       <Header />
-      <div className='list-items'>
-        <Section />
+      <div className="list-items">
+        <Section
+          countery="Japan"
+          location="Mount Fuji"
+          coverImg="/japan.png"
+          mapLink="https://maps.app.goo.gl/Ao4FRXomBBuqwF1L6"
+          startDate="12 Jan, 2024"
+          endDate="24 Jan, 2024"
+          placeSummary="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
+        <Section
+          countery="Japan"
+          location="Mount Fuji"
+          coverImg="/japan.png"
+          mapLink="https://maps.app.goo.gl/Ao4FRXomBBuqwF1L6"
+          startDate="12 Jan, 2024"
+          endDate="24 Jan, 2024"
+          placeSummary="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
+        <Section
+          countery="Japan"
+          location="Mount Fuji"
+          coverImg="/japan.png"
+          mapLink="https://maps.app.goo.gl/Ao4FRXomBBuqwF1L6"
+          startDate="12 Jan, 2024"
+          endDate="24 Jan, 2024"
+          placeSummary="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
       </div>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,33 +3,31 @@ import location from "../assets/location.svg";
 import Japan from "/japan.png";
 
 function Section(props) {
-
+//   console.log(props);
 
   return (
     <section className="item">
       <div className="section--main">
-        <img src={Japan} className="section--image" />
+        <img src={props.coverImg} className="section--image" />
 
         <div className="subsection">
           <div className="header--location">
             <span>
               <img src={location} alt="Location" />
-              <span className="countery">#japan</span>
-              <a href="http://mac:5173" className="map--text">
+              <span className="countery">{props.countery}</span>
+              <a href={props.mapLink} className="map--text">
                 View on Google Maps
               </a>
             </span>
           </div>
 
-          <h1 className="location">Mount Fuji</h1>
+          <h1 className="location">{props.location}</h1>
 
-          <h3 className="date">12 Jan, 2021 - 24 Jan, 2021</h3>
+          <h3 className="date">
+            {props.startDate} - {props.endDate}
+          </h3>
 
-          <p className="brief-text">
-            Mount Fuji is the tallest mountain in Japan, standing at 3,776
-            meters (12,380 feet). Mount Fuji is the single most popular tourist
-            site in Japan, for both Japanese and foreign tourists.
-          </p>
+          <p className="brief-text">{props.placeSummary}</p>
         </div>
       </div>
 
